7e4b67fdefb62a6739ed6f3a9701fdd7b4a69798
Mattia Tamellini <mattia.tamellini@gmail.com>
2014-03-19 18:24:23 +0100

    Added ComparisonFunctors in src/Utils, modified some CMakeLists.txt and begun working on CompositeDifferentialProblem

---
+++ src/DifferentialProblem/CompositeDifferentialProblem.hpp
@@ -0,0 +1,13 @@
+#ifndef SRC_DIFFERENTIALPROBLEM_COMPOSITEDIFFERENTIALPROBLEM_HPP_INCLUDE_GUARD
+#define SRC_DIFFERENTIALPROBLEM_COMPOSITEDIFFERENTIALPROBLEM_HPP_INCLUDE_GUARD
+
+#include <DifferentialProblem/AbstractDifferentialProblem.hpp>
+#include <DifferentialProblem/LinearDifferentialProblem.hpp>
+#include <DifferentialProblem/NonLinearDifferentialProblem.hpp>
+#include <dolfin.h>
+#include <map>
+#include <set>
+#include <tuple>
+
+
+#endif
---
+++ src/Utils/ComparisonFunctors.hpp
@@ -0,0 +1,31 @@
+#ifndef SRC_UTILS_COMPARISONFUNCTORS_HPP_INCLUDE_GUARD
+#define SRC_UTILS_COMPARISONFUNCTORS_HPP_INCLUDE_GUARD
+
+/*! This file contains definitions of the function object \c control_problem::less, 
+ * \c control_problem::greater, \c control_problem::equal_to, ... so that we can use them in 
+ * standard library containers that need an ordering relationship throughout this
+ * project. We prefer not to specialize comparison operators in std library (e.g. std::less<T>,
+ * std::greater<T>, ...) because otherwise projects that use this library will also inherit 
+ * such specializations
+ */
+#include <functional>
+#include <tuple>
+#include <string>
+
+namespace control_problem
+{
+	/*! \class less
+	 *  \brief class that implements the "less than" comparison. 
+	 *  
+	 *  Its call operator returns true if first argument is less than the second argument, 0 otherwise
+	 *  This is not a template class. Instead, call operator is overloaded for every type we need in this
+	 *  project. If you want more generality, use std::less<T>
+	 */
+	class less
+	{
+		public:
+			bool operator() (const std::tuple <std::string, std::string, std::string>& lhs, 
+			                 const std::tuple <std::string, std::string, std::string>& rhs);
+	};
+}
+#endif

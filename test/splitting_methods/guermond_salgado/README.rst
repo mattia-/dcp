1a01567acc32b92491ea41bf394da66dd449cfc7
Mattia Tamellini <mattia.tamellini@gmail.com>
2015-02-02 12:21:03 +0100

    GuermondSalgadoMethod constructor now takes as input argument the initial value of the density. Also added a test on GuermondSalgadoMethod

---
+++ test/splitting_methods/guermond_salgado/README.rst
@@ -0,0 +1,18 @@
+This collection of files sets up the case for a test splitting method
+problem. We will use the Navier-Stokes equations with the Guermond-Salgado
+splitting to study the 2D flow in the unit disk. 
+See J.-L. Guermond, A. Salgado, A splitting method for incompressible flows with variable
+density ..., J. Comput. Phys. (2009), doi:10.1016/j.jcp.2008.12.036, section 5.1
+
+Steps to launch the code:
+
+1) compile::
+
+    mkdir build
+    cd build
+    cmake ..
+    make
+
+2) run the executable::
+
+    ./src/main
---
+++ test/splitting_methods/guermond_salgado/src/main.cpp
@@ -0,0 +1,251 @@
+/* 
+ *  Copyright (C) 2014, Mattia Tamellini, mattia.tamellini@gmail.com
+ * 
+ *  This file is part of the DCP library
+ *   
+ *   The DCP library is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The DCP library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with the DCP library.  If not, see <http://www.gnu.org/licenses/>. 
+ */ 
+
+#include <iostream>
+#include <string>
+#include <dolfin.h>
+#include <mshr.h>
+#include <dcp/differential_problems/differential_problems.h>
+#include <dcp/splitting_methods/splitting_methods.h>
+#include "density.h"
+#include "velocity.h"
+#include "pressure_correction.h"
+#include "pressure_update.h"
+#include "error_computers.h"
+
+namespace navierstokes
+{
+    class InitialRho : public dolfin::Expression
+    {
+        void eval (dolfin::Array<double>& values, const dolfin::Array<double>& x) const override
+        {
+            values[0] = sin (x[0]);
+        }
+    };
+    
+    // the external force is divided into two parts: f = f1 * rho + f2
+    // this is because rho is a solution, and there is no way to link it
+    // to a TimeDependentExpression
+    class FirstExternalLoad : public dcp::TimeDependentExpression
+    {
+        public:
+            FirstExternalLoad () : TimeDependentExpression (2) { }
+        
+            void eval (dolfin::Array<double>& values, const dolfin::Array<double>& x) const override
+            {
+                values[0] =  x[1] * sin (t) - x[0] * cos (t) * cos (t);
+                values[1] = -x[0] * sin (t) - x[1] * cos (t) * cos (t);
+            }
+    }; 
+    
+
+    class SecondExternalLoad : public dcp::TimeDependentExpression
+    {
+        public:
+            SecondExternalLoad () : TimeDependentExpression (2) { }
+
+            void eval (dolfin::Array<double>& values, const dolfin::Array<double>& x) const override
+            {
+                values[0] =  cos (x[0]) * sin (x[1]) * sin (t);
+                values[1] =  sin (x[0]) * cos (x[1]) * sin (t);
+            }
+    }; 
+    
+    
+    class NoSlipBoundary : public dolfin::SubDomain
+    { 
+        bool inside (const dolfin::Array<double>& x, bool on_boundary) const
+        {
+            return on_boundary;
+        }
+    }; 
+}
+
+namespace exact_solutions
+{
+    class Rho : public dcp::TimeDependentExpression
+    {
+        void eval (dolfin::Array<double>& values, const dolfin::Array<double>& x) const override
+        {
+            values[0] = 2 + x[0] * cos (sin (t)) + x[1] * sin (sin (t));
+        }
+    };
+    
+    class U : public dcp::TimeDependentExpression 
+    {
+        public:
+            U () : TimeDependentExpression (2) { }
+            
+            void eval (dolfin::Array<double>& values, const dolfin::Array<double>& x) const override
+            {
+                values[0] = -x[1] * cos (t);
+                values[1] =  x[0] * cos (t);
+            }
+    };
+    
+    class P : public dcp::TimeDependentExpression
+    {
+        void eval (dolfin::Array<double>& values, const dolfin::Array<double>& x) const override
+        {
+            values[0] = sin (x[0]) * sin (x[1]) * sin (t);
+        }
+    };
+}
+
+int main (int argc, char* argv[])
+{
+//    dolfin::set_log_level (dolfin::DBG);
+    // create mesh and finite element space
+    std::cout << "Create mesh and finite element space..." << std::endl;
+    mshr::Circle circle (dolfin::Point (0.0, 0.0), 1);
+    dolfin::Mesh mesh;
+    mshr::generate (mesh, circle, 50);
+    
+    density::FunctionSpace R (mesh);
+    velocity::FunctionSpace V (mesh);
+    pressure_correction::FunctionSpace Q (mesh);
+
+    // define constant
+    std::cout << "Define the coefficients..." << std::endl;
+//    dolfin::Constant initialRho (1);
+    navierstokes::InitialRho initialRho;
+    dolfin::Constant mu (1e-1);
+    dolfin::Constant chi (1);
+    dolfin::Constant noSlipDirichletBC (0.0, 0.0);
+    double t0 = 0.0;
+    double dt = 0.1;
+    double T = 0.2; //10;
+    
+    // define the problems
+    std::cout << "Define the problem..." << std::endl;
+    dcp::GuermondSalgadoMethod <density::BilinearForm,
+                                density::LinearForm,
+                                velocity::BilinearForm,
+                                velocity::LinearForm,
+                                pressure_correction::BilinearForm,
+                                pressure_correction::LinearForm,
+                                pressure_update::BilinearForm,
+                                pressure_update::LinearForm>
+        guermondSalgadoMethod (R, V, Q, t0, dt, T, mu, chi, initialRho);
+    
+    
+    // define dirichlet boundary conditions
+    std::cout << "Define the problem's Dirichlet boundary conditions..." << std::endl;
+    navierstokes::NoSlipBoundary noSlipBoundary;
+    
+    std::cout << "Setting up dirichlet's boundary conditions for 'velocity_problem'" << std::endl;
+    guermondSalgadoMethod.addDirichletBC ("velocity_problem", noSlipDirichletBC, noSlipBoundary);
+
+    
+    // define time dependent external loads
+    navierstokes::FirstExternalLoad f1;
+    navierstokes::SecondExternalLoad f2;
+    guermondSalgadoMethod.problem ("velocity_problem").addTimeDependentCoefficient 
+        ("f1", 
+         "linear_form", 
+         dolfin::reference_to_no_delete_pointer (f1));
+    guermondSalgadoMethod.problem ("velocity_problem").addTimeDependentCoefficient 
+        ("f2", 
+         "linear_form", 
+         dolfin::reference_to_no_delete_pointer (f2));
+    guermondSalgadoMethod.system ().addLink ("velocity_problem", "rho", "linear_form", "density_problem");
+        
+    
+    for (auto& name : guermondSalgadoMethod.system ().problemsNames ())
+    {
+        guermondSalgadoMethod.problem (name).parameters ["plot_interval"] = 1;
+        guermondSalgadoMethod.problem (name).parameters ["plot_title"] = name;
+    }
+    
+    // plots
+    dolfin::plot (mesh, "Mesh");
+    
+    // solve problem
+    std::cout << "Solve the problem..." << std::endl;
+    guermondSalgadoMethod.apply ();
+
+    // ------------- //
+    // compute error //
+    // ------------- //
+    
+    // exact solution
+    exact_solutions::Rho exactRho; 
+    exact_solutions::U exactU; 
+    exact_solutions::P exactP; 
+    
+    // computed solution
+    auto computedRho = guermondSalgadoMethod.problem ("density_problem").solutionsWithTimes ();
+    auto computedU = guermondSalgadoMethod.problem ("velocity_problem").solutionsWithTimes ();
+    auto computedP = guermondSalgadoMethod.problem ("pressure_update_problem").solutionsWithTimes ();
+    
+    // error computers
+    error_computers::Form_density_L2_squared_error densityL2SquaredErrorComputer (mesh);
+    error_computers::Form_velocity_L2_squared_error velocityL2SquaredErrorComputer (mesh);
+    error_computers::Form_velocity_H1_squared_error velocityH1SquaredErrorComputer (mesh);
+    error_computers::Form_pressure_L2_squared_error pressureL2SquaredErrorComputer (mesh);
+    
+    // vectors to contain errors, so that we can get the maximum error later
+    std::vector<double> densityL2Errors (computedRho.size ());
+    std::vector<double> velocityL2Errors (computedU.size ());
+    std::vector<double> velocityH1Errors (computedU.size ());
+    std::vector<double> pressureL2Errors (computedP.size ());
+    
+    std::cout << "Computing errors..." << std::endl;
+    for (std::size_t i = 0; i < computedRho.size (); ++i)
+    {
+        const double& time = computedRho [i].first;
+            
+        exactRho.setTime (time);
+        exactU.setTime (time);
+        exactP.setTime (time);
+        
+        densityL2SquaredErrorComputer.exact_rho = exactRho;
+        velocityL2SquaredErrorComputer.exact_u = exactU;
+        velocityH1SquaredErrorComputer.exact_u = exactU;
+        pressureL2SquaredErrorComputer.exact_p = exactP;
+    
+        densityL2SquaredErrorComputer.rho = *(computedRho[i].second);
+        velocityL2SquaredErrorComputer.u = *(computedU[i].second);
+        velocityH1SquaredErrorComputer.u = *(computedU[i].second);
+        pressureL2SquaredErrorComputer.p = *(computedP[i].second);
+    
+        densityL2Errors[i] = sqrt (dolfin::assemble (densityL2SquaredErrorComputer));
+        velocityL2Errors[i] = sqrt (dolfin::assemble (velocityL2SquaredErrorComputer));
+        velocityH1Errors[i] = sqrt (dolfin::assemble (velocityH1SquaredErrorComputer));
+        pressureL2Errors[i] = sqrt (dolfin::assemble (pressureL2SquaredErrorComputer));
+    }
+    std::cout << "done" << std::endl;
+    
+    double maxDensityL2Error = *(std::max_element (densityL2Errors.begin (), densityL2Errors.end ()));
+    double maxVelocityL2Error = *(std::max_element (velocityL2Errors.begin (), velocityL2Errors.end ()));
+    double maxVelocityH1Error = *(std::max_element (velocityH1Errors.begin (), velocityH1Errors.end ()));
+    double maxPressureL2Error = *(std::max_element (pressureL2Errors.begin (), pressureL2Errors.end ()));
+        
+    std::cout << "Max density L2 error: " << maxDensityL2Error << std::endl;
+    std::cout << "Max velocity L2 error: " << maxVelocityL2Error << std::endl;
+    std::cout << "Max velocity H1 error: " << maxVelocityH1Error << std::endl;
+    std::cout << "Max pressure L2 error: " << maxPressureL2Error << std::endl;
+    // ----------------- //
+    // end compute error //
+    // ----------------- //
+    
+    dolfin::interactive ();
+    
+    return 0;
+}
